8bddb012db01b8dcb494ae6ce038749396ee9a69
Sijmen Koffeman <koffemans@hotmail.com>
2020-12-01 14:02:08 +0000

    now count is  in state too

--- BlazorAppState/Services/AppState.cs
+++ BlazorAppState/Services/AppState.cs
@@ -16,7 +16,7 @@ namespace BlazorAppState.Services
 
         public string MyProperty { get; private set; } = $"initialized on {DateTime.Now} by {nameof(AppState)}";
 
-        public int MyProperty2 { get; private set; } = 100;
+        public int MyProperty2 { get; private set; } = 0;
 
         public void UpdateMyProperty(object source, string value)
         {
